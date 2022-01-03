2ef7f2fbb8d7d5df731fc078c60ae0b419cb387e
techthejoshuastudio <tech@thejoshuastudio>
2022-01-03 23:08:52 +0000

    Complete advanced testing setup

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -11,4 +11,10 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     get static_pages_help_url
     assert_response :success
   end
+  
+  test "should get help" do
+    get static_pages_about_url
+    assert_response :success
+  end
 end
+
