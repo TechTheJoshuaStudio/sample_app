86d301fe6f789e494e09ba9251a199ab970e7269
techthejoshuastudio <tech@thejoshuastudio>
2022-01-03 23:14:01 +0000

    Complete advanced testing setup

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,7 +4,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   
   test "should get home" do
     get static_pages_home_url
-    assert_response :success
+    assert_response :redirect
   end
 
   test "should get help" do
