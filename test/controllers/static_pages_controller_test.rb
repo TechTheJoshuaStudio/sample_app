86b54fe256262b2635bc85415e8e9d7135e96f0d
techthejoshuastudio <tech@thejoshuastudio>
2022-01-09 18:29:28 +0000

    Finish Layout and routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,23 +1,28 @@
-require "test_helper"
+require 'test_helper'
 
-class StaticPagesControllerTest < ActionController::TestCase
-  
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
   test "should get home" do
-    get :home
+    get root_path
     assert_response :success
     assert_select "title", "Ruby on Rails Tutorial Sample App"
   end
 
   test "should get help" do
-    get :help
+    get help_path
     assert_response :success
     assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
   end
-  
+
   test "should get about" do
-    get :about
+    get about_path
     assert_response :success
     assert_select "title", "About | Ruby on Rails Tutorial Sample App"
   end
-end
 
+  test "should get contact" do
+    get contact_path
+    assert_response :success
+    assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
+  end
+end
