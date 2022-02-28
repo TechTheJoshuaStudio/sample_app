c2823bfb3f9476a63a50802990189f993af05092
The Joshua Studio <tech@thejoshuastudio.com>
2022-02-28 06:43:09 -0600

    Add password reset

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -13,6 +13,17 @@ class UserMailerTest < ActionMailer::TestCase
     assert_match CGI.escape(user.email),  mail.body.encoded
   end
 
+  test "password_reset" do
+    user = users(:michael)
+    user.reset_token = User.new_token
+    mail = UserMailer.password_reset(user)
+    assert_equal "Password reset", mail.subject
+    assert_equal [user.email], mail.to
+    assert_equal ["josh@thejoshuastudio.com"], mail.from
+    assert_match user.reset_token,        mail.body.encoded
+    assert_match CGI.escape(user.email),  mail.body.encoded
+  end
+
   # test "password_reset" do
   #   mail = UserMailer.password_reset
   #   assert_equal "Password reset", mail.subject
